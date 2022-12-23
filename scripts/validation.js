d7f03736c96cceddebf31517d3133e79e2e0e166
0502j <jamiledsousa@gmail.com>
2022-12-23 16:28:18 -0300

    Update project - removing unused scripts & adding validation script

---
+++ scripts/validation.js
@@ -0,0 +1,32 @@
+let form1success = false;
+const radio = document.querySelectorAll('input[name="radio-opt"]');
+
+function checkRadio(){
+    for(let i=0;i<radio.length;i++){
+        if(radio[i].checked){
+            if(validateSelected() !== 'not-selected'){
+                form1success=true;
+                window.location = "investments.html";
+            }
+        }
+    }
+
+    if(form1success===false){
+        alert("Please fill the form before going to the next page.");
+        window.location = "account-status.html";
+    }
+}
+
+
+function validateSelected(){
+    let selection = document.getElementById('sel');
+    let seloption = selection.options[selection.selectedIndex];
+    let value = seloption.value;
+
+    console.log(value);
+
+    return value;
+}
+
+
+
